70cf0ec370478a161e6b5a694a16f0907d46a8c6
Marcin Kukliński <senghe@gmail.com>
2019-03-24 12:24:42 +0100

    Configuring react

---
+++ assets/js/WeatherMap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class WeatherMap extends React.Component {
+    render() {
+        return (
+            <div>
+                WeatherMap
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<WeatherMap />, document.getElementById('root'));
